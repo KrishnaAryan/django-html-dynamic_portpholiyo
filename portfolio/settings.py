a9594bdc6049a0c8896a0cdd83f282b00f2c0705
krishna <krishnaaryan369@gmail.com>
2023-04-18 15:10:04 +0530

    Krishna

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=$ohni9+_gm1z(8juehs1(ge@g)&%ohhxrraml$@(4=q35b5%_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
