9ffa88fe25d3a1daa4308486745e1bcceca094d6
krishna <krishnaaryan369@gmail.com>
2023-04-18 16:33:23 +0530

    Krishna

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,8 @@
 
 from django.urls import path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('',views.index, name='index'),
     path('contact',views.contact, name='contact'),
@@ -12,5 +12,4 @@ urlpatterns = [
     
     
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns +=staticfiles_urlpatterns()
